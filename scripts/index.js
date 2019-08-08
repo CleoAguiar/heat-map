11228d232d6bcc58274137040e18e86a61db72e7
Cleo Aguiar <cleotaisas@gmail.com>
2019-08-08 11:52:20 -0300

    feat: add title id. User Story #1

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
-	console.log('first script');
+	var margin = {top: 100, botton: 30, left: 60, rigth: 20},
+		width = 920 - margin.left - margin.rigth,
+		height = 600 - margin.top - margin.botton;
+
+	var svg = d3.select('.heatMap')
+				.append('svg')
+				.attr('width', width + margin.left + margin.rigth)
+				.attr('height', height + margin.top + margin.botton)
+				.append('g');
+
+	// Title
+	svg.append('text')
+	   .attr('id', 'title')
+	   .attr('x', width/2)
+	   .attr('y', margin.top / 2)
+	   .text('Monthly Global Land-Surface Temperature');
 });
