3170463b87d48c08befb7377c4cda5d8551b89e4
Cleo Aguiar <cleotaisas@gmail.com>
2019-08-09 20:57:25 -0300

    feat: add id axis. User Story #3 and #4

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+	var url = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json';
 	var margin = {top: 100, botton: 30, left: 60, rigth: 20},
 		width = 920 - margin.left - margin.rigth,
 		height = 600 - margin.top - margin.botton;
@@ -20,5 +21,17 @@ $(document).ready(function(){
 					.attr('width', width + margin.left + margin.rigth)
 					.attr('height', height + margin.top + margin.botton)
 					.append('g');
+
+	d3.json(url).then(function(data){
+		// xAxis
+		svg.append('g')
+			.attr('class', 'x-axis')
+			.attr('id', 'x-axis');
+
+		// yAxis
+		svg.append('g')
+			.attr('class', 'y-axis')
+			.attr('id', 'y-axis');
+	});
 
 });
